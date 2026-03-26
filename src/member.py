14c580effce284745c1a942ebc28e35d439c0d4b
PotatoReds <JonathanSJ@outlook.dk>
2026-03-26 11:24:35 +0100

    Added member class

--- src/member.py
+++ src/member.py
@@ -1,17 +1,20 @@
+# member class // self = itself, a reference to current instance of class // self.borrowed_books = [] = creates an empty list for borrowed_books attribute when a member is created
 class Member:
-    def __init__(self, member_id, name, email, borrowed_books):
+    def __init__(self, member_id, name, email):
         self.member_id = member_id
         self.name = name
         self.email = email
-        self.borrowed_books = borrowed_books = []
+        self.borrowed_books = []
 
+    # method to display member information in a structured return format
     def display_info(self):
-        return f"{self.member_id} - {self.name}, Borrowed books: {self.borrowed_books}"
-        return f"Contact info: {self.email}"
+        return f"{self.member_id} - {self.name}, Borrowed books: {self.borrowed_books}, Contact info: {self.email}"
     
+    # method for borrowing a book, adds the book_id to member borrowed_books list // append = add to the list
     def borrow_book(self, book_id):
         self.borrowed_books.append(book_id)
 
+    # method for returning a book, removes book_id from member borrowed_books list
     def return_book(self, book_id):
         if book_id in self.borrowed_books:
             self.borrowed_books.remove(book_id)
